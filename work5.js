6b4b98b227b4274283b6ad64cec9bf7982c31e65
Soumya Mugalolli <soumyamugalolli@gmail.com>
2026-08-21 10:12:42 +0530

    Delete work5.js

--- work5.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config()
-const http=require("http")
-const server=http.createServer((req,resp)=>{
-    resp.writeHead(200,{"content-type":"text/html"})
-    resp.write(`<h1>${process.env.SERVER} has been started`)
-    resp.end()
-})
-server.listen(process.env.PORT,()=>{console.log("Server has been started")})
