dbc0d742aa5099f255fdd78ea395be886dc50ea9
Soumya Mugalolli <soumyamugalolli@gmail.com>
2026-08-21 10:19:27 +0530

    Delete hello.js

--- hello.js
+++
@@ -1,9 +0,0 @@
-const http=require("http");
-const server=http.createServer((req,resp)=>{
-   resp.writeHead(200,{"content-type":"text/html"});
-   resp.write("<h1>BLDEA's comm BHS Arts & TGP Science College</h1>");
-   resp.write("<h2>Started Year 1963</h2>");
-   resp.write("<h3>Located in Jamakhandi</h3>");
-   resp.end();
-});
-server.listen(3000,()=>{console.log("server has been started")})
